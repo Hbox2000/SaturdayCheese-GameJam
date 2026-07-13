f5a880cddf545935fc6bb9f37ff1bd2020893f93
Hbox2000 <h_raiss@icloud.com>
2026-07-14 08:58:43 +1000

    Made growing and seeds flying

--- Scripts/plant.gd
+++ Scripts/plant.gd
@@ -1,21 +1,23 @@
 extends Node2D
 
+@onready var sprite_2d: Sprite2D = $Sprite2D
+@onready var grow_time: Timer = $GrowTimer
+
 var _plantTextureId : int = 0
 var _plantStage: int = 0
-var _plantMaxStage: int = 4;
-
-#Getting reference to sprite
-@onready var sprite_2d: Sprite2D = $Sprite2D
+var _plantMaxStage: int = 4
+var _plantWatered: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	sprite_2d.texture = getTexture()
+	grow_time.wait_time = randi_range(10, 15)
+	grow_time.timeout.connect(grow)
+	grow_time.start()
 
 # Basically tick
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
-	if Input.is_action_just_pressed("ui_right"):
-		grow()
 	
 #Gets the texture from the cropManager using the texture ID and the plant's current stage
 func getTexture() -> Resource:
@@ -23,14 +25,16 @@ func getTexture() -> Resource:
 	
 #Increases the plantStage by one
 func grow() -> void:
-	if (_plantStage < _plantMaxStage):
+	if _plantStage < _plantMaxStage and _plantWatered:
 		_plantStage += 1
-		sprite_2d.texture = cropManager.getTexture(_plantTextureId, _plantStage)
-		
-	else:
-		push_error("Plant stage was somehow over the limit when calling grow()")
+		sprite_2d.texture = getTexture()
 	
-	
+	grow_time.wait_time = randi_range(10, 15)
+	grow_time.start()
+
 #Returns the plant's current stage
 func getPlantStage() -> int:
 	return _plantStage
+
+func waterPlant() -> void:
+	_plantWatered = true
--- Scripts/window.gd
+++ Scripts/window.gd
@@ -4,6 +4,8 @@ const seedScene = preload("res://Scenes/seed.tscn")
 
 @onready var window: Area2D = $"."
 @onready var collision_shape: CollisionShape2D = $CollisionShape2D
+@onready var seed_timer: Timer = $SeedTimer
+
 var windowBounds: RectangleShape2D
 var numberOfPlants: int
 var centerPoint: Vector2
@@ -12,18 +14,19 @@ var centerPoint: Vector2
 func _ready() -> void:
 	windowBounds = collision_shape.shape as RectangleShape2D
 	numberOfPlants = cropManager.maxSeedCount() - 1
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_up"):
-		spawnSeed(0)
+	seed_timer.wait_time = randf_range(0.5, 1)
+	seed_timer.timeout.connect(spawnSeed.bind(randi_range(0, cropManager.maxSeedCount() - 1)))
+	seed_timer.start()
 
 func spawnSeed(seedId: int) -> void:
 	var newSeed = seedScene.instantiate()
-	newSeed.seedTextureId = 0
+	newSeed.seedTextureId = seedId
 	newSeed.windowBounds = windowBounds
 	newSeed.windowCenter = position
-	newSeed.position.y = randi_range(collision_shape.position.y - windowBounds.size.y / 2, collision_shape.position.y + windowBounds.size.y / 2)
+	newSeed.position.y = randf_range(collision_shape.position.y - windowBounds.size.y / 2, collision_shape.position.y + windowBounds.size.y / 2)
 	newSeed.position.x = collision_shape.position.x - windowBounds.size.x / 2 - 20
 	
 	add_child(newSeed)
+	
+	seed_timer.wait_time = randf_range(0.5, 1)
+	seed_timer.start()
